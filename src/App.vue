3e92afb0c380243592fbc2ad564142d6175be995
Edwin Boon <edwin.boon@autoflex.nl>
2018-05-18 15:52:32 +0200

    Add SearchBar to view and component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
-        Hello World!
+        <SearchBar></SearchBar>
     </div>
 </template>
 
 <script>
+import SearchBar from './components/SearchBar';
+
 export default {
-    name: 'App'
+    name: 'App',
+    components: {
+        SearchBar
+    }
 };
 </script>
