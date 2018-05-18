deac4832c919b2fad2bbd519d377b28edd502f9a
Edwin Boon <edwin.boon@autoflex.nl>
2018-05-18 16:39:52 +0200

    Add axios and api_key for youtube

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
 
 <script>
 import SearchBar from './components/SearchBar';
+const API_KEY = 'AIzaSyCdByZNG1KORNJ_SGdl_tCmoc4FMkEot6g';
 
 export default {
     name: 'App',
@@ -14,6 +15,7 @@ export default {
     },
     methods: {
         newSearch(searchTerm) {
+            console.log(API_KEY)
             console.log(searchTerm);
         }
     }
